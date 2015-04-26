bde477937df4b6558f9ada85b7539b519e40da04
Christian Richard <lifepupil@gmail.com>
2015-04-25 20:51:25 -0700

    more config stuff

--- client/index.js
+++ client/index.js
@@ -3,5 +3,7 @@
 angular.module('blog', ['firebase'])
 .config(['$stateProvider','$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
 	$stateProvider
-	.state('home',{url: '/', templateUrl: /views/home/home.html})
+	.state('home',{url: '/', templateUrl: '/views/home/home.html'})
+  .state('login', {url: '/login', templateUrl: '/views/users/users.html', controller: 'UsersCtrl'});
+
 }]);

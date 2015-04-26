721a30247013b172d04845970f8339624cacb6ef
Christian Richard <lifepupil@gmail.com>
2015-04-25 20:22:35 -0700

    create state controller and home page

--- client/index.js
+++ client/index.js
@@ -1,3 +1,7 @@
 'use strict';
 
-angular.module('poseidon', ['firebase']);
+angular.module('blog', ['firebase'])
+.config(['$stateProvider','$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+	$stateProvider
+	.state('home',{url: '/', templateUrl: /views/home/home.html})
+}]);
